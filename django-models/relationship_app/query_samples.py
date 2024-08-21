40b0dbdb673c86f6fd12df61124b165e3f3bb079
sisay27 <sisayanmaw27@gmail.com>
2024-08-21 22:43:29 +0300

    commiting 10-b

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,4 +1,3 @@
-# relationship_app/query_samples.py
 
 from .models import Author, Book, Library, Librarian
 
@@ -6,7 +5,9 @@ from .models import Author, Book, Library, Librarian
 specific_author_books = Book.objects.filter(author__name='Specific Author')
 
 # List all books in a library
-all_books_in_library = Book.objects.filter(library__name='Library Name')
+library_name = 'Library Name'
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
 
 # Retrieve the librarian for a library
-librarian_for_library = Librarian.objects.get(library__name='Library Name')
+librarian_for_library = Librarian.objects.get(library__name=library_name)
