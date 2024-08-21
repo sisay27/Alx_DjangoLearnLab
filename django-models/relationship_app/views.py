73ca5c3306235e4e401aeecb599908052449898d
sisay27 <sisayanmaw27@gmail.com>
2024-08-21 23:30:58 +0300

    commiting 10-second

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book
+from django.views.generic import ListView
+from .models import Library
 
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+class LibraryDetailView(ListView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
 # Create your views here.
