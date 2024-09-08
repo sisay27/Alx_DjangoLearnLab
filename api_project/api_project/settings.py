035bdeec86e0b9691cfc314dc6e6548927949f3f
sisay27 <sisayanmaw27@gmail.com>
2024-09-08 22:18:11 +0300

    4th api

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api', 
+    'rest_framework.authtoken',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
