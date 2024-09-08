9dd50eeb335eb283291dc9ace48cab12bd547879
sisay27 <sisayanmaw27@gmail.com>
2024-09-08 22:12:32 +0300

    3rd-aapi

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,13 @@
 # api_project/urls.py
-
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
 from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('api.urls')),
 ]
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = router.urls
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
