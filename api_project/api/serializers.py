7e930145c5fefb10f92ae1f95738be0a21eb4ef7
sisay27 <sisayanmaw27@gmail.com>
2024-09-08 20:58:49 +0300

    commiting -2nd api

---
+++ api_project/api/serializers.py
@@ -0,0 +1,9 @@
+# api_project/api/serializers.py
+
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
---
+++ api_project/api/urls.py
@@ -0,0 +1,8 @@
+# api_project/api/urls.py
+
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
