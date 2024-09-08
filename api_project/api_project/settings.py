5c8f2443cd133cbcd415bf41610599706dcb9e77
sisay27 <sisayanmaw27@gmail.com>
2024-09-08 22:28:11 +0300

    commiting 4th

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
