24e53cb952c474e4a1f8535c181aeefa3ece4a71
sisay27 <sisayanmaw27@gmail.com>
2024-09-08 22:30:55 +0300

    commiting-2nd

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -20,3 +20,9 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import path, include
+
+urlpatterns = [
+    # Other URL patterns for your project
+    path('api/', include('api.urls')),
+]
