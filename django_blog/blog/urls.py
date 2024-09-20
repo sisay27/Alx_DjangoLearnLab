cf9c832d26a00dcd453aaf6f905e343b64cd1dd9
sisay27 <sisayanmaw27@gmail.com>
2024-09-20 16:17:05 +0300

    commiting-blog-2-cr1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
@@ -8,13 +15,12 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
 ]
 
-
 app_name = 'blog'
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post_list'),
     path('posts/new/', PostCreateView.as_view(), name='post_create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
