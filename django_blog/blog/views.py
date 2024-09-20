5c4a01ea8165e5aa201e555035e4b42e33195296
sisay27 <sisayanmaw27@gmail.com>
2024-09-20 11:01:20 +0300

    commiting-blog-1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def profile_view(request):
+    user = request.user
+    if request.method == 'POST':
+        user.email = request.POST.get('email')
+        # Update other profile fields as needed
+        user.save()
+        # Redirect to the profile page or a success page
+        return redirect('profile')
+    context = {
+        'user': user
+    }
+    return render(request, 'app_name/profile.html', context)
