01fd1001d9311a47f9a8239381b23df6b480976f
sisay27 <sisayanmaw27@gmail.com>
2024-09-20 20:36:11 +0300

    2-4

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,9 @@ from django.views.generic import ListView,DetailView
 from .models import Post
 from django.views.generic.edit import DeleteView,CreateView,UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 
 @login_required
 def profile_view(request):
@@ -30,21 +33,30 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
     context_object_name = 'post'
-class PostCreateView(CreateView):
+
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_form.html'
-    fields = ['title', 'content']  # Fields to display in the form
+    fields = ['title', 'content']
 
-class PostUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'post_form.html'
-    fields = ['title', 'content']  # Fields to display in the form
+    fields = ['title', 'content']
 
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
-
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'post_confirm_delete.html'
-    success_url = reverse_lazy('post_list')  # Redirect URL after deletion
-    
+    success_url = reverse_lazy('post_list')
 
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
