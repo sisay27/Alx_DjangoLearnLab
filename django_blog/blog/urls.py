1765d85bb1e7742dfb34d35a2cc4c0938c7c32e4
sisay27 <sisayanmaw27@gmail.com>
2024-09-20 20:29:31 +0300

    2-3

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
 ]
