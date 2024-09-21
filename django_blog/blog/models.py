b7dc67f18ac5f520d7f4a465b737017271d1fb8d
sisay27 <sisayanmaw27@gmail.com>
2024-09-21 10:06:30 +0300

    blog3-3

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,24 @@
 from django.db import models  
 from django.contrib.auth.models import User  
 from blog.models import Post
+from taggit.managers import TaggableManager
 
 class Post(models.Model):  
     title = models.CharField(max_length=200)  
     content = models.TextField()  
     published_date = models.DateTimeField(auto_now_add=True)  
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # Handles multiple posts by a single author
+
+class Tag( models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -17,3 +29,9 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    tags = TaggableManager()
