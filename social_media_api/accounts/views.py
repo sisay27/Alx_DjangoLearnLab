af87f7f285adcad8f7b6bf2ba6a797b58734bf89
sisay27 <sisayanmaw27@gmail.com>
2024-09-28 10:58:05 +0300

    cm-3-2

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -30,7 +30,20 @@ class UserListView(generics.ListAPIView):
         users = self.get_queryset()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
 
+    def get(self, request, user_id):
+        user = get_object_or_404(CustomUser, id=user_id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 User = get_user_model()  
 
